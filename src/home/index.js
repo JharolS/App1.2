3fed9ebdc51849c720aa13ac7d9e58d705be7eb8
unknown <sv73107535@idat.edu.pe>
2021-10-30 20:38:34 -0500

    first commit

---
+++ src/home/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import {
+    Text,
+    View,
+    StyleSheet
+} from "react-native";
+
+const Home = () => {
+    return(
+        <View style={styles.container}>
+            
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default Home;
